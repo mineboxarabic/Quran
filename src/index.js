980c2954337efad561bb5c204cda05d545ce2395
Yassin YOUNES <mineboxarabic@gmail.com>
2022-10-06 20:38:33 +0200

    f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import "./stylesheet.css"
-console.log('test')
-let body = document.querySelector('body')
-let h = document.createElement('h1')
-h.textContent = 'testtttt'
-body.appendChild(h)
+import quranJSON from './quran.json'
+function Quran()
+{
+    this.Sourahs = quranJSON
+    this.sourahsIndex = []
+    let i = 0
+    this.Sourahs.forEach(sourah => {
+        this.sourahsIndex.push(sourah.name)
+        console.log(`sourahMap.set(${sourah.name},${i})`)
+        i++
+    })
+    //console.log(this.sourahsIndex)
+    this.getSourah = function(name)
+    {
+        const sourahMap = new Map()
+        for(let i = 0 ; i < 100 ; i++)
+        {
+            sourahMap.set(this.sourahsIndex[i], i)
+        }
+        return this.Sourahs[sourahMap.get(name)];
+    };
+};
+let quran = new Quran;
+console.log(quran.getSourah('البقرة'))

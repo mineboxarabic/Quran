fa570316ad3c968c3c556d7ff42e953067614674
Yassin YOUNES <mineboxarabic@gmail.com>
2022-10-06 17:08:45 +0200

    After that it works

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./stylesheet.css"
+console.log('test')
+let body = document.querySelector('body')
+let h = document.createElement('h1')
+h.textContent = 'testtttt'
+body.appendChild(h)
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,40 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { Template } = require("webpack");
+const CleanwebpackPlugin = require("clean-webpack-plugin")
 
-
-module.exports
+module.exports = 
 {
-    
-}
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'main.js',
+      clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({template: './src/index.html'})],
+
+    module: 
+    {
+        rules:
+        [
+            {
+                test: /\.html$/,
+                use:
+                [
+                    "html-loader"
+                ]
+            },
+                    
+            {
+                test: /\.css$/,
+                use:
+                ["style-loader","css-loader"]
+
+            }
+        ]
+
+        
+    }
+
+};

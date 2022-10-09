24b6255698301310ecc1e334c57f668750dbecdd
Yassin YOUNES <mineboxarabic@gmail.com>
2022-10-09 10:47:08 +0200

    the last one

--- src/index.js
+++ src/index.js
@@ -20,24 +20,4 @@ input.addEventListener("keyup",function lookForSourah(event)
 {
     let name = input.value
     quran.showSourah(name,theSourahDiv)
-    /*if(quran.isSourah(name))
-    {
-        console.log('true')
-        for(let i = 0 ; i < quran.getSourah(name).length; i++){
-            
-        let p = quran.getSourah(name)[i];
-        console.log(p)
-        theSourahDiv.appendChild(p)
-        }
-    }
-    else
-    {
-        if(theSourahDiv.hasChildNodes())
-        {
-            removeAllChildNodes(theSourahDiv)
-        }
-        
-
-    }*/
-    
 })
